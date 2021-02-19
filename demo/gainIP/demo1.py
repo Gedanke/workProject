f37228ee63e8e8af1133c276d05e2daee712dad1
Gedanke <13767927306@163.com>
2021-02-19 22:56:10 +0800

    photo

--- demo/gainIP/demo1.py
+++ demo/gainIP/demo1.py
@@ -1,8 +1,31 @@
 # -*- coding:utf-8 -*-
 
 
-from selenium import webdriver
+import time
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 
-browser = webdriver.PhantomJS()
-browser.get('https://www.baidu.com')
-print(browser.current_url)
+chrome_options = Options()
+chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
+chrome_options.add_experimental_option('useAutomationExtension', False)
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-blink-features-AutomationControlled")
+chrome_options.add_argument(
+    'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36')
+
+driver = Chrome('chromedriver', options=chrome_options)
+driver.set_window_size(1366, 768)
+with open('/home/dfs/repository/py/stealth.min.js-main/stealth.min.js') as f:
+    js = f.read()
+
+driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    "source": js
+})
+# driver.get('https://www.zbytb.com/s-jian-136965.html')
+driver.get('https://bot.sannysoft.com/')
+
+# 你可以保存源代码为 html 再双击打开，查看完整结果 https://bot.sannysoft.com/
+source = driver.page_source
+print(source)
+with open('result.html', 'w') as f:
+    f.write(source)
